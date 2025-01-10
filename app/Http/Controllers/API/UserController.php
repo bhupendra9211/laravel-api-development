7ace5d28b8482ca38caed7f747facdacf0de73cc
bhupendra9211 <shahbhupendra9211@gmail.com>
2025-01-10 10:55:56 +0545

    implemented sanntum

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 use Exception;
 
 class UserController extends Controller
@@ -61,8 +62,9 @@ class UserController extends Controller
                 'password' => bcrypt($request->password)
             ]
         );
+        $token = $user->createToken($request->name);
         if ($user->id) {
-            $result = array('status' => true, 'message' => 'User created', 'data' => $user);
+            $result = array('status' => true, 'message' => 'User created', 'data' => $user,'token' => $token->plainTextToken);
             return response()->json($result, 200); // Success response
         } else {
             $result = array('status' => false, 'message' => 'User not created');
@@ -105,11 +107,78 @@ class UserController extends Controller
             return response()->json($result,$responseCode);
         }
 
+
         $user->delete();
 
         $result = array('status'=>true,'message'=>'user deleted sucessfully');
         $responseCode = 200;
 
         return response()->json($result,$responseCode);
+    }
+
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $result = array(
+                'status' => false,
+                'message' => 'Validation errors occurred',
+                'error_message' => $validator->errors()
+            );
+            return response()->json($result, 400); // Bad request
+        }
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return [
+                'errors' => [
+                    'email' => ['The provided credentials are incorrect.']
+                ]
+            ];
+            // return [
+            //     'message' => 'The provided credentials are incorrect.' 
+            // ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+
+        // $credentials = $request->only("email","password");
+
+        // if(Auth::attempt($credentials)){
+        //     $user = Auth::user();
+
+            // $token = $user->createToken('MyApp')->accessToken;
+            // $token = $user->createToken('token-name', ['server:update'])->plainTextToken;
+            // $token = $user->createToken('MyApp')->plainTextToken;
+
+            // $result = array('status'=>true,'message'=>'Login sucessfully',"data"=>$user);
+            // $responseCode = 200;
+    
+            // return response()->json($result,$responseCode);
+
+
+        // }else{
+
+            // $result = array('status'=>false,'message'=>'Invalid Login');
+            // $responseCode = 401;
+    
+            // return response()->json($result,$responseCode);
+        // }
+
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return [
+            'message' => 'You are logged out.' 
+        ];
     }
 }
